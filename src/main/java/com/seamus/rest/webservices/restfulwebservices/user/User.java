4b2d8aa67a7aa96d7f9b578d14bb51f35040d206
stynan27 <seamustynan16@gmail.com>
2023-03-26 21:21:31 -0400

    Customizing JSON field names

--- src/main/java/com/seamus/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/seamus/rest/webservices/restfulwebservices/user/User.java
@@ -2,6 +2,8 @@ package com.seamus.rest.webservices.restfulwebservices.user;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
@@ -10,9 +12,11 @@ public class User {
 	private Integer id;
 	
 	@Size(min=2, message = "Name should have at least 2 characters")
+	@JsonProperty("user_name")
 	private String name;
 	
 	@Past(message = "Birthdate should be in the past")
+	@JsonProperty("birth_date")
 	private LocalDate birthdate;
 	
 	public User(Integer id, String name, LocalDate birthdate) {
